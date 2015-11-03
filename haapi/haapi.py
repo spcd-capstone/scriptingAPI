5a29f810075348ef6265979ccea4d9ba7b9d282c
stvc <stevecain7@gmail.com>
2015-11-03 10:54:03 -0600

    some slight progress

--- haapi/haapi.py
+++ haapi/haapi.py
@@ -1,12 +1,40 @@
 import contextlib
 import socket
 
-@contextlib.contextmanager
-def nodeConnection(nodeIP, nodePort):
-    s = socket.socket()
-    try:
-        s.connect((nodeIP, nodePort))
-        yield
-    finally:
-        s.close()
+
+class NodeNotConnected(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+
+class NodeConnection:
+    def __init__(self, nodeIP, nodePort):
+        self.nodeIP = nodeIP
+        self.nodePort = nodePort
+        self.sock = socket.socket()
+        self.isConnected = False
+
+    def __enter__(self):
+        try:
+            self.sock.connect((self.nodeIP, self.nodePort))
+            self.isConnected = True
+            return self
+        except Exception as e:
+            print("Could not connect. Exception type is %s".format(type(e).__name__))
+            raise e
+
+    def __exit__(self, type, value, traceback):
+        self.sock.close()
+        self.isConnected = False
+
+    def __del__(self):
+        self.sock.close()
+
+    def setVal(self, key, value):
+        if not self.isConnected:
+            raise NodeNotConnected("Never connected to node ({}, {})".format(self.nodeIP, self.nodePort))
+
+
 
--- tests/test_tcp.py
+++ tests/test_tcp.py
@@ -2,6 +2,8 @@ import socket
 import threading
 import unittest
 
+from multiprocessing.pool import ThreadPool
+
 from haapi import haapi
 
 
@@ -15,14 +17,33 @@ class TestTCPConnections(unittest.TestCase):
         conn.close()
         server_sock.close()
 
-    def test_entering_with_connects(self):
+    def test_connection_error_raises_exception(self):
+        with self.assertRaises(Exception):
+            with haapi.NodeConnection('127.0.0.1', 7777):
+                pass
+
+    def test_entering_context_connects(self):
         server_thread = threading.Thread(target=self.run_fake_server)
         server_thread.start()
 
-        with haapi.nodeConnection('127.0.0.1', 7777):
+        with haapi.NodeConnection('127.0.0.1', 7777):
             pass
 
         server_thread.join()
+
+    def test_entering_context_returns_object(self):
+        server_thread = threading.Thread(target=self.run_fake_server)
+        server_thread.start()
+
+        with haapi.NodeConnection('127.0.0.1', 7777) as c:
+            self.assertFalse(c == None)
+
+        server_thread.join()
+
+    def test_sending_when_not_connected_raises_NodeNotConnected(self):
+        node = haapi.NodeConnection('127.0.0.1', 7777)
+        with self.assertRaises(haapi.NodeNotConnected):
+            node.setVal('on', 1)
 
 
 if __name__ == '__main__':
